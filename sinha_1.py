300e8386ed45206d5e79c015cd0b6a2f1fa72a7d
hectorterceros-usp <hector.terceros@usp.br>
2020-06-01 16:42:09 -0300

    Avançando na construção desse modelo.
    Consegui montar o esqueleto para o processo de decisão.
    Já consigo, por Lesk e Degree, escolher um synset para cada palavra.
    Mas os dados que eu tenho do Senseval-2 estão com outros labels.
    Não sei como cruzar esses labels com o synset escolhido.
    Terei que procurar isso, e também montar as outras técnicas.
    E olha que eu estou ainda usando bastante de bibliotecas.
    Além do mais, precisarei fazer a melhoria da visualização.
    Acho que consigo resolver a maior parte dessas coisas entre hoje e amanhã.

--- sinha_1.py
+++ sinha_1.py
@@ -23,7 +23,7 @@ def clean_context(instance):
 
 clean_instance = clean_context(instance)
 # para dar uma limpada nos resultados
-clean_instance = clean_instance[:4]
+# clean_instance = clean_instance[:4]
 
 def get_synsets(clean_instance):
     synsets = []
@@ -32,7 +32,8 @@ def get_synsets(clean_instance):
         new_synsets = wn.synsets(w, pos=l)
         if len(new_synsets) < 1:
             print('não encontrou synsets para "{}"'.format(w))
-        new_synsets = {'word': w, 'position':p, 'synsets': new_synsets}
+        # new_synsets = [{'w': w, 's': s for s in new_synsets]
+        # new_synsets = {'word': w, 'synsets': new_synsets}
         synsets.append(new_synsets)
     return synsets
 
@@ -43,17 +44,18 @@ len(clean_instance)
 np.prod([len(s) for s in synsets if len(s) > 0])
 # só uma palhinha da importância de resolver essa ambiguidade
 
-def get_sim_matrix(synsets, dependency, max_dist = 3):
+def get_sim_graph(synsets, dependency, max_dist = 3):
     G = nx.DiGraph()
     l = len(synsets)
     for i in range(l):
         for j in range(i, l):
             if j - i > max_dist:
                 break
-            for t in synsets[i]['synsets']:
-                for s in synsets[j]['synsets']:
+            for t in synsets[i]:
+                for s in synsets[j]:
                     weight = dependency(t, s)
                     if weight > 0:
+                        G
                         G.add_edge(t, s, weight = weight)
     return G
 
@@ -62,10 +64,29 @@ def lesk(x, y):
         set(x.definition().split()) &
         set(y.definition().split()))
 
-G = get_sim_matrix(synsets, lesk)
+G = get_sim_graph(synsets, lesk)
 nx.draw(G, with_labels=True)
 plt.plot()
 
 len(G)
 len(G.edges(data='weight'))
-nx.degree_centrality(G)
+
+def score_vertices(G, centrality):
+    return centrality(G)
+
+# vertices = nx.degree_centrality(G)
+vertices = score_vertices(G, nx.degree_centrality)
+
+def assign_label(clean_instance, synsets, vertices):
+    assigned = []
+    for i in range(len(clean_instance)):
+        values = [vertices[s] for s in synsets[i]]
+        if len(values) < 1:
+            chosen = ''
+        else:
+            chosen = synsets[i][np.argmax(values)]
+        assigned.append((clean_instance[i][0], chosen))
+    return assigned
+
+result = assign_label(clean_instance, synsets, vertices)
+result
